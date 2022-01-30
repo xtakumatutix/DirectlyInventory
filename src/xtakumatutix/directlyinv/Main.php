216bd74e89210d946631a5cefa54eb56e23b0fb3
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2022-01-30 14:30:30 +0900

    て"き"た"

---
+++ src/xtakumatutix/directlyinv/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace xtakumatutix\directlyinv;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\utils\Config;
+
+Class Main extends PluginBase implements Listener
+{
+    public function onEnable() :void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->Config = new Config($this->getDataFolder() . "Config.yml", Config::YAML, array(
+            'メッセージ' => '§bインベントリがいっぱいのため下に落としました',
+        ));
+    }
+
+    public function onBreak(BlockBreakEvent $event)
+    {
+        $player = $event->getPlayer();
+        $dropitem = $event->getDrops();
+        $event->setDrops([]);
+        foreach($dropitem as $item){
+            
+            if ($player->getInventory()->canAddItem($item))
+            {
+                $player->getInventory()->addItem($item);
+            }else{
+                $world = $player->getWorld();
+                $position = $player->getPosition();
+                $world->dropItem($position, $item);
+                $player->sendPopup($this->Config->get('メッセージ'));
+            }
+        }
+    }
+}
