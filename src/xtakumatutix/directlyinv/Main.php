133942ab20b3a0b19c790f779c1f03f1d9d348f5
famima65536 <famima65536@gmail.com>
2022-01-31 23:09:13 +0900

    インデント修正

--- src/xtakumatutix/directlyinv/Main.php
+++ src/xtakumatutix/directlyinv/Main.php
@@ -11,16 +11,16 @@ use pocketmine\event\block\BlockBreakEvent;
 
 Class Main extends PluginBase implements Listener
 {
-	private bool $useStackStorage = false;
+    private bool $useStackStorage = false;
 
     public function onEnable() :void
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->saveDefaultConfig();
+        $this->saveDefaultConfig();
 
-		if($this->getServer()->getPluginManager()->getPlugin("StackStorage") !== null){
-			$this->useStackStorage = true;
-		}
+        if($this->getServer()->getPluginManager()->getPlugin("StackStorage") !== null){
+            $this->useStackStorage = true;
+        }
 
     }
 
@@ -28,51 +28,51 @@ Class Main extends PluginBase implements Listener
     {
         $player = $event->getPlayer();
         $dropitem = $event->getDrops();
-		$event->setDrops([]);
+        $event->setDrops([]);
 
-		if($this->useStackStorage and !$this->getConfig()->getNested("stackstorage.player-inventory-first")){
-			$this->sendItemsToStackStorage($player, $dropitem);
-			return;
-		}
+        if($this->useStackStorage and !$this->getConfig()->getNested("stackstorage.player-inventory-first")){
+            $this->sendItemsToStackStorage($player, $dropitem);
+            return;
+        }
 
-		$remainder = $this->tryAddItemToInventory($player->getInventory(), $dropitem);
-		if(count($remainder) > 0){
-			if($this->useStackStorage){
-				$this->sendItemsToStackStorage($player, $remainder);
-				$message = $this->getConfig()->getNested("message.inventory-full-with-stackstorage");
-			}else{
-				$event->setDrops($remainder);
-				$message = $this->getConfig()->getNested("message.inventory-full");
-			}
-			$player->sendPopup($message);
-		}
+        $remainder = $this->tryAddItemToInventory($player->getInventory(), $dropitem);
+        if(count($remainder) > 0){
+            if($this->useStackStorage){
+                $this->sendItemsToStackStorage($player, $remainder);
+                $message = $this->getConfig()->getNested("message.inventory-full-with-stackstorage");
+            }else{
+                $event->setDrops($remainder);
+                $message = $this->getConfig()->getNested("message.inventory-full");
+            }
+            $player->sendPopup($message);
+        }
     }
 
-	/**
-	 * @param Inventory $inventory
-	 * @param Item[] $items
-	 * @return Item[] 追加されなかった分のアイテム
-	 */
-	private function tryAddItemToInventory(Inventory $inventory, array $items): array{
-		$remainder = [];
+    /**
+     * @param Inventory $inventory
+     * @param Item[] $items
+     * @return Item[] 追加されなかった分のアイテム
+     */
+    private function tryAddItemToInventory(Inventory $inventory, array $items): array{
+        $remainder = [];
         foreach($items as $item){
             if ($inventory->canAddItem($item))
-			{
-				$inventory->addItem($item);
-			}else{
-				$remainder[] = $item;
-			}
-		}
-		return $remainder;
-	}
+            {
+                $inventory->addItem($item);
+            }else{
+                $remainder[] = $item;
+            }
+        }
+        return $remainder;
+    }
 
-	/**
-	 * @param Player $player
-	 * @param Item[] $items
-	 */
-	private function sendItemsToStackStorage(Player $player, array $items){
-		foreach($items as $item){
-			StackStorageAPI::$instance->add($player->getXuid(), $item);
-		}
-	}
+    /**
+     * @param Player $player
+     * @param Item[] $items
+     */
+    private function sendItemsToStackStorage(Player $player, array $items){
+        foreach($items as $item){
+            StackStorageAPI::$instance->add($player->getXuid(), $item);
+        }
+    }
 }
